5f28ff2abdbf3d91ded464facbb26150245dc47b
oniri1 <oniri1@naver.com>
2024-09-02 15:06:17 +0900

    feat : redirect to main when 404 paging

---
+++ front/src/app/not-found.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/");
+  }, [router]);
+
+  return <div>404</div>;
+};
+
+export default NotFound;
